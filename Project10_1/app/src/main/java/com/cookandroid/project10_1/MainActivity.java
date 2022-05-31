8cbe51fc926063d90bbec2d7f94b684ee987df25
truthsoloud <truthsoloud@gachon.ac.kr>
2022-05-31 10:36:46 +0900

    class vote application

---
+++ Project10_1/app/src/main/java/com/cookandroid/project10_1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.cookandroid.project10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnNewActivity=(Button)findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
